cc1ba472b12475d4190dbb4ebf542796d00028f5
balajepalli84 <165810247+balajepalli84@users.noreply.github.com>
2025-03-21 11:44:08 -0500

    Delete variables.tf

--- variables.tf
+++
@@ -1,8 +0,0 @@
-variable tenancy_ocid     = "ocid1.tenancy.oc1..aaaaaaaaa3qmjxr43tjexx75r6gwk6vjw22ermohbw2vbxyhczksgjir7xdq"
-variable user_ocid        = "ocid1.user.oc1..aaaaaaaafaq7vh72xyy3gj47vooic6hi7a2w2tvkzplgcjlohe7aawa4n6ea"
-variable fingerprint      = "65:a4:ea:9a:6b:fc:60:70:10:88:35:9b:70:18:d4:f5"
-variable private_key_path = "~/.oci/private-key.pem"
-variable region= "us-ashburn-1"
-variable "compartment_ocid" = "ocid1.compartment.oc1..aaaaaaaafklcekq7wnwrt4zxeizcrmvhltz6wxaqzwksbhbs73yz6mtpi5za"
-variable "subnet_ocid" ="ocid1.subnet.oc1.iad.aaaaaaaavbogtxo5uxelricigx4jm6nw77xaannxi35v3dpmtorlzzlfjvqq"
-variable "ssh_key_path" {}
